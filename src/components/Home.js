4eafbcb734bac1c62e1b312dba43137b9c2a1de6
Raghav9525 <kshubham9525.gmail.com>
2024-03-17 20:09:52 +0530

    commi

--- src/components/Home.js
+++ src/components/Home.js
@@ -27,19 +27,17 @@ function Home() {
               </div>
             </div>
 
-            {/* <div className="carousel-item active" style={{ position: "relative" }}>
-              <img src={require(`./img/bac3.jpg`)} className="d-block w-100" alt="..." />
+            <div className="carousel-item active" style={{ position: "relative" }}>
+              <img src={require(`./img/LocationBac1.jpg`)} className="d-block w-100" alt="..." />
               <div className="text" style={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)", textAlign: "center" }}>
 
-                <p className="fw-bold d-none d-md-block" style={{ fontSize: "8vh", color: "white", fontFamily: "sans-serif" }}>We Make Your Clothes Fresh and Clean</p>
-                <p className="fw-bold d-block d-md-none" style={{ fontSize: "2vh", color: "white", fontFamily: "sans-serif" }}>We Make Your Clothes Fresh and Clean</p>
-
+                {/* button for desktop screen */}
                 <div className='d-flex justify-content-center' >
-                  <button className="btn bg-primary p-2 ps-4 pe-4   btn-lg" style={{ color: "white", fontWeight: "600" }} onClick={() => navigate('/order')}>Order</button>
-                  <button className="btn p-2 btn-lg ms-2" style={{ backgroundColor: "rgb(149,87,198)", color: "white", fontWeight: "600" }} onClick={() => navigate(`/delivery`)}>Delivery</button>
+                  <button className="btn bg-primary p-2 ps-4 pe-4   btn-lg" style={{ color: "white", fontWeight: "600" }} onClick={() => navigate('/login')}>Login</button>
+                  <button className="btn p-2 btn-lg ms-4" style={{ backgroundColor: "rgb(149,87,198)", color: "white", fontWeight: "600" }} onClick={() => navigate(`/signup`)}>Signup</button>
                 </div>
               </div>
-            </div> */}
+            </div>
 
 
             <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
